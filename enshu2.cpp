a29e810b86102e202e4d52d90c838cc62615584c
Your Name <you@example.com>
2017-10-23 18:33:08 +0900

    hoge

--- enshu2.cpp
+++ enshu2.cpp
@@ -1,50 +1,62 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
-#include <vector>
+#include <string.h>
 #include <sys/time.h>
 
-int n = 1000;
+int n = 1024;
 
-void printall(std::vector<std::vector<float> > u)
+void printall(float u[])
 {
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
-            std::cout << std::setw(2) << (int)(u[i][j]*10);
+            std::cout << std::setw(2) << (int)(u[i*n+j]*10);
         }
         std::cout << std::endl << std::endl;
     }
 }
 
+void init(float* u){
+    for(int i=0; i<n; i++)
+    {
+        u[i]=0;
+        u[i*n]=0;
+        u[n*(n-1)+i]=0;
+        u[i*n+n-1]=0;
+
+    }
+	for(int i=1; i<n-1; i++)
+    {
+		for(int j=1; j<n-1; j++)
+        {
+			u[i*n+j]=1;
+		}
+	}
+}
+
 int main()
 {
-    struct timeval t0, t1;
-
-
     float r = 0.25f;
-    std::vector<std::vector<float> > u(n,std::vector<float>(n,1));
-    for(int i=0; i<n; i++){
-        u[0][i]=0;
-        u[n-1][i]=0;
-        u[i][0]=0;
-        u[i][n-1]=0;
-    }
-    std::vector<std::vector<float> > u2 = u;
-    //printall(u);
+    float u[n*n];
+    float u2[n*n];
+    init(u);
+    memcpy(u2, u, sizeof(float)*n*n);
+
+    struct timeval t0, t1;
     gettimeofday(&t0,NULL);
     for(int i=0; i<100; i++){
         #pragma omp parallel for
         for(int k=1; k<n-1; k++){
             #pragma omp parallel for
             for(int l=1; l<n-1; l++){
-                u2[k][l] = (1-4*r)*u[k][l]+r*(u[k+1][l]+u[k-1][l]+u[k][l+1]+u[k][l-1]);
+                u2[k*n+l] = (1-4*r)*u[k*n+l]+r*(u[(k+1)*n+l]+u[(k-1)*n+l]+u[k*n+l+1]+u[k*n+l-1]);
             }
         }
-        u=u2;
+        memcpy(u, u2, sizeof u2);
     }
     gettimeofday(&t1,NULL);
     std::cout << "100steps later:" << std::endl;
-   // printall(u);
+    //printall(u);
     std::cout << "100 steps, u:" << n << "x" << n << std::endl;
     std::cout << "time: " << (double)(t1.tv_sec - t0.tv_sec)+(double)(t1.tv_usec - t0.tv_usec)*1.0e-6 << std::endl;
 
--- enshu2.cu
+++ enshu2.cu
@@ -1,35 +1,84 @@
-#include <stdio.h>
-#include <cuda.h>
-#include <cuda_runtime.h>
 #include <iostream>
-#include <vector>
+#include <iomanip>
 #include <string>
+#include <string.h>
+#include <sys/time.h>
 
-__device__ void device_strcpy(char *dst, const char *src)
+int n = 1024;
+
+void printall(float u[])
 {
-	while(*dst++ = *src++);
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            std::cout << std::setw(2) << (int)(u[i*n+j]*10);
+        }
+        std::cout << std::endl << std::endl;
+    }
 }
 
-__global__ void kernel(char *A)
-{
-	device_strcpy(A, "testtest");
+void init(float* u){
+    for(int i=0; i<n; i++)
+    {
+        u[i]=0;
+        u[i*n]=0;
+        u[n*(n-1)+i]=0;
+        u[i*n+n-1]=0;
+
+    }
+	for(int i=1; i<n-1; i++)
+    {
+		for(int j=1; j<n-1; j++)
+        {
+			u[i*n+j]=1;
+		}
+	}
 }
 
-int main(){
-	char* d_hello;
-	char hello[32];
+__device__ void device_step(float* u2, float* u1)
+{
+	if(threadIdx==0 || threadIdx==n-1)
+		return;
 
-	cudaMalloc((void**)&d_hello, 32);
+	for(int k=1; k<n-1; k++)
+	{
+		u2[k*n+threadIdx] = (1-4*r)*u1[k*n+threadIdx]+r*(u1[(k+1)*n+threadIdx]+u1[(k-1)*n+threadIdx]+u1[k*n+threadIdx+1]+u1[k*n+threadIdx-1]);
+	}
+}
 
-	kernel<<<1,1>>>(d_hello);
+__global__ void kernel(float* u2, float* u1)
+{
+	device_step(u2,u1);
+}
 
-	cudaMemcpy(hello, d_hello, 32, cudaMemcpyDeviceToHost);
+int main()
+{
+    float r = 0.25f;
+    float u1[n*n];
+    float u2[n*n];
+    init(u1);
+    memcpy(u2, u1, sizeof(float)*n*n);
 
-	cudaFree(d_hello);
+	float* d_u1;
+	float* d_u2;
+	cudaMalloc((void**)&d_u1,sizeof(float)*n*n);
 
-	puts(hello);
+	cudaMemcpy(d_u1, u1, sizeof(float)*n*n, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_u2, u2, sizeof(float)*n*n, cudaMemcpyHostToDevice);
 
+    struct timeval t0, t1;
+    gettimeofday(&t0,NULL);
+    for(int i=0; i<100; i++){
+        kernel<<<1,1024>>>(d_u2,d_u1);
+		cudaMemcpy(u2, d_u2, sizeof(float)*n*n, cudaMemcpyDeviceToHost)
+        memcpy(u, u2, sizeof u2);
+    }
+    gettimeofday(&t1,NULL);
+	cudaFree(d_u1);
+	cudaFree(d_u2);
+    std::cout << "100steps later:" << std::endl;
+    //printall(u);
+    std::cout << "100 steps, u:" << n << "x" << n << std::endl;
+    std::cout << "time: " << (double)(t1.tv_sec - t0.tv_sec)+(double)(t1.tv_usec - t0.tv_usec)*1.0e-6 << std::endl;
 
-
-	return 0;
+    return 0;
 }
