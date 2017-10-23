8582d8fea55de78108f1b0068e65dab6e111bffe
Your Name <you@example.com>
2017-10-23 21:28:51 +0900

    hoge

--- enshu2.cu
+++ enshu2.cu
@@ -36,11 +36,12 @@ void init(float* u){
 }
 __global__ void kernel(float* u2, float* u1, int N, int R)
 {
+	N=32; R=0.1;
 	for(int i=0; i<100; i++){
 		if(threadIdx.x/N!=0 && threadIdx.x/N!=N-1 && threadIdx.x%N!=0 && threadIdx.x%N!=N-1)
 		{
-			u2[threadIdx.x] = ui[threadIdx.x]+1;
-//			u2[threadIdx.x] = (1-4*R)*u1[threadIdx.x] + R*(u1[threadIdx.x+N]+u1[threadIdx.x-N]+u1[threadIdx.x+1]+u1[threadIdx.x-1]);
+			//u2[threadIdx.x] = threadIdx.x;
+			u2[threadIdx.x] = (1-4*R)*u1[threadIdx.x] + R*(u1[threadIdx.x+N]+u1[threadIdx.x-N]+u1[threadIdx.x+1]+u1[threadIdx.x-1]);
 		}
 		__syncthreads();
 		u1[threadIdx.x] = u2[threadIdx.x];
